880380189f2177a9ccfb8df25304d39d4efae973
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-11-05 17:41:36 +0900

    Delete router-react-app directory

--- router-react-app/src/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-function NotFound(){
-    return (<Navigate to ="/"/>)
-}
-
-export default NotFound
--- router-react-app/src/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-function PrivateRoute ({element, isAuthenticated}){
-    return isAuthenticated ? element : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
