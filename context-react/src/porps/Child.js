3b639885f5b00afafa3e75d123ceb09ed7c54222
dongugchoi <163122705+dongugchoi@users.noreply.github.com>
2024-11-05 17:40:47 +0900

    Delete context-react directory

--- context-react/src/porps/Child.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GrandChild from './GrandChild';
-
-function Child() {
-  return <GrandChild />;
-}
-
-export default Child;
--- context-react/src/porps/Parent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Child from './Child';
-
-function Parent({ user }) {
-  return <Child />;
-}
-
-export default Parent;
