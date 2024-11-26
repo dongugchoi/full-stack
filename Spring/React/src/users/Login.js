ea3c740d6fa5e3411567daab9c105b560630d074
dongugchoi <ehddnr7321@gmail.com>
2024-11-26 17:52:18 +0900

    Update Login.js

--- Spring/React/src/users/Login.js
+++ Spring/React/src/users/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
                         </td>
                     </tr>
                 </tbody>
-            </table>
+            </table >
             
         </form>
     )
